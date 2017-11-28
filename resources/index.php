083e7e51a64debccf0b38cd9ff38519634a50599
Subhadip Chowdhury <subhadip@math.uchicago.edu>
2017-11-28 16:27:46 -0600

    updated modular structure

---
+++ resources/index.php
@@ -0,0 +1,46 @@
+<html style="-moz-box-shadow:inset 0 0 20px #000000; -webkit-box-shadow:inset 0 0 20px #000000; box-shadow:inset 0 0 20px #000000;">
+	<head>
+		<?php  include 'includes/metadata.html'; ?> 
+	</head>
+
+	<body>
+	<div class="main">
+		<?php  include 'includes/navmenu.html'; ?> 
+		<hr><hr>
+		
+		<div class="anchor" id="notes">
+			<h2>Notes/Articles</h2>
+			<h3>First Year materials</h3>
+			Look <a href="firstyear.html">here</a>.
+			<h3>Articles/Notes for seminars or projects </h3>
+			<ul>
+				<li> An expository article on <a href="./coursedocuments/Homework_shared/Analysis III HW/finalassignment.pdf">Siegel Discs</a> as MATH 31400 (Spring 13) Analysis - 3: Final project.
+				<li><a href="./Talks/Summer 2013/Rigidity Seminar notes(unfinished).pdf"> Notes </a> for my talk in the Rigidity Seminar (Summer 2013) regarding Gromov's proof of  Mostow Rigidity theorem.
+			</ul>
+		</div>
+		
+		<!--
+		<hr>
+
+		<div class="anchor" id="links">
+			<h2>Seminars, Math Blogs and Other Informative Links</h2>
+			<ul>
+			<li>Uchicago Math department <a href="http://www.math.uchicago.edu/seminars/geometry_topology.html">Geometry/Topology Seminar</a>
+			<li>Uchicago Math department <a href="http://math.uchicago.edu/~dynamics/website.html">Dynamics Seminar</a>
+			<li><a href="http://farbandfriends.wordpress.com/"> Farb and Friends </a> Student Seminar.
+			<li>Notes from <a href="http://math.uchicago.edu/~cbutler/rigidity.html" target="_blank"> Rigidity Theory Seminar</a> (Summer 2013).
+			<li>Latex-ed class notes for 1st Year courses : <a href="http://math.uchicago.edu/~chonoles/">Zev's website</a>. 
+			<li>Notes from seminars and helpful study guides : <a href="http://math.uchicago.edu/~papisa/notes/" target="_blank">Paul's website</a>.
+			<li>Prof. Danny Calegari's blog : <a href="http://lamington.wordpress.com/"> Geometry and the imagination </a>
+			</ul>
+		</div>
+		<hr><hr>
+		-->
+		<!--<div style="height: 20px; background: url(./load.gif) 50% 50% no-repeat transparent;"></div>-->
+	</div>
+	
+	<footer>
+	<? php  include 'includes/footer.html'; ?> 
+	</footer>
+	</body>
+</html>
