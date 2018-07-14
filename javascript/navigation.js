512026abe79e798f14eb186c8d34fc4873579986
Subhadip Chowdhury <subhadip@math.uchicago.edu>
2018-07-13 19:56:35 -0500

    js edit

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
     $("#nav-mobile").html($("#nav-main").html());
     $("#nav-trigger span").click(function () {
       if ($("nav#nav-mobile ul").hasClass("expanded")) {
-        $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-        $(".wrapper-menu").removeClass("open");
+          $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+          $(".wrapper-menu").removeClass("open");
       } 
       else {
         $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
         $(".wrapper-menu").addClass("open");
       }
     });
+});
+
