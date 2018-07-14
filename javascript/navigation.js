c6b5aa3133f884b696011d0ec7b527a5631da2ed
Subhadip Chowdhury <subhadip@math.uchicago.edu>
2018-07-13 19:34:13 -0500

    nav css js update

---
+++ javascript/navigation.js
@@ -0,0 +1,14 @@
+/*Toggles the navigation menu for small devices*/
+
+$(document).ready(function () {
+    $("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger span").click(function () {
+      if ($("nav#nav-mobile ul").hasClass("expanded")) {
+        $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+        $(".wrapper-menu").removeClass("open");
+      } 
+      else {
+        $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+        $(".wrapper-menu").addClass("open");
+      }
+    });
