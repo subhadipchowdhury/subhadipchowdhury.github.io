a024a30d5023122e657718a227f3b99e13f0f3b4
subhadipchowdhury <subhadipnet@gmail.com>
2018-07-22 11:45:17 -0500

    js fixed

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,7 +1,6 @@
 /*Toggles the navigation menu for small devices*/
 
 $(document).ready(function () {
-    $("#nav-mobile").html($("#nav-main").html());
     $("#nav-trigger span").click(function () {
       if ($("nav#nav-mobile ul").hasClass("expanded")) {
           $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
